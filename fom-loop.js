617234cd36463b3314a5a792e02c8691e9e405e6
udayagiri-sarathchandra <u.sarathchandra9>
2021-07-10 14:09:30 +0530

    8th july Tasks on forms

---
+++ fom-loop.js
@@ -0,0 +1,46 @@
+// var name = document.getElementById("first");
+// var nameicon = document.getElementsByClassName("bi bi-x-circle");
+
+/************************************************************************************************/
+
+// $myFunction()
+// {
+//     $('button').on({
+//         mouseover: function () { $('#first').show() },
+//         click: function () { $('#first').hide() }
+//     })
+// }
+
+var Movie_songs = [
+    {
+        image: "first",
+        Audio: "bi bi-x-circle",
+        title: "name",
+        text: "nameicon",   //0
+        date: "block",
+        music: "block",
+        vd: "none",
+        vvd: "none"
+    },
+    {
+        image: "first",
+        Audio: "bi bi-x-circle",
+        title: "name",
+        text: "nameicon",   //0
+        date: "block",
+        music: "block",
+        vd: "none",
+        vvd: "none"
+    },
+    {
+        image: "first",
+        Audio: "bi bi-x-circle",
+        title: "name",
+        text: "nameicon",   //0
+        date: "block",
+        music: "block",
+        vd: "none",
+        vvd: "none"
+    }
+
+]
